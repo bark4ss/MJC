84e016833a40f2709be2afdfbb5bffffd98da30c
bark4ss <v.telyatko@gmail.com>
2020-06-01 23:56:58 +0300

    Update benchmarks

--- src/main/java/benchmark/BenchmarkCollection.java
+++ src/main/java/benchmark/BenchmarkCollection.java
@@ -12,21 +12,24 @@ import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
 @Fork(value = 2, jvmArgs = { "-Xms2G", "-Xmx2G" })
+//@Measurement(iterations = 10)
+//@Warmup(iterations = 5)
 public class BenchmarkCollection {
 
-	@Param({ "10000" })
-	private static int N;
+	private static long N = 1_000_000;
 	private static final String STRING_TO_FIND = String.valueOf(N - N / 4);
 
 	private List<String> linkedList;
@@ -54,27 +57,27 @@ public class BenchmarkCollection {
 	}
 
 	@Benchmark
-	public void List_LinkedList() {
-		linkedList.contains(STRING_TO_FIND);
+	public void List_LinkedList(Blackhole blackhole) {
+		blackhole.consume(linkedList.contains(STRING_TO_FIND));
 	}
 
 	@Benchmark
-	public void List_ArrayList() {
-		arrayList.contains(STRING_TO_FIND);
+	public void List_ArrayList(Blackhole blackhole) {
+		blackhole.consume(arrayList.contains(STRING_TO_FIND));
 	}
 
 	@Benchmark
-	public void Set_HashSet() {
-		hashSet.contains(STRING_TO_FIND);
+	public void Set_HashSet(Blackhole blackhole) {
+		blackhole.consume(hashSet.contains(STRING_TO_FIND));
 	}
 
 	@Benchmark
-	public void Set_LinkedHashSet() {
-		linkedHashSet.contains(STRING_TO_FIND);
+	public void Set_LinkedHashSet(Blackhole blackhole) {
+		blackhole.consume(linkedHashSet.contains(STRING_TO_FIND));
 	}
 
 	@Benchmark
-	public void Set_TreeSet() {
-		treeSet.contains(STRING_TO_FIND);
+	public void Set_TreeSet(Blackhole blackhole) {
+		blackhole.consume(treeSet.contains(STRING_TO_FIND));
 	}
 }
